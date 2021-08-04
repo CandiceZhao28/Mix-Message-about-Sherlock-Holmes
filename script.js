3a5c54ae7fb1a6946b9a8b18f9e92c1571460ed3
Candice Zhao <candice.zyq@gmail.com>
2021-08-04 11:39:11 +0100

    add a blank line above the message

--- script.js
+++ script.js
@@ -3,11 +3,10 @@ const obj = {
     doesWhat: ['committed suicide', 'was found dead', 'was murdered', 'was kidnapped by criminals', 'served in a war', 'moved into a flat', 'spied for money', 'shot a cabbie', 'had a sponsor', 'smuggled an antique Chinese Vase', 'found a dead man\' body'],
     where: ['on Baker Street', 'in Afghanistan', 'at a bank in the City', 'at The British Museum', 'near the Waterloo Bridge', 'by the swimming pool', 'at St Bartholomew\'s Hospotal'],
     message() {
-        const rand1 = Math.floor(Math.random()*this.who.length);  
-        const rand2 = Math.floor(Math.random()*this.doesWhat.length);
-        const rand3 = Math.floor(Math.random()*this.where.length);
-        const msg = this.who[rand1] + ' ' + this.doesWhat[rand2] + ' ' + this.where[rand3] + '...';
-        console.log(msg);
+        let rand1 = Math.floor(Math.random()*this.who.length);  
+        let rand2 = Math.floor(Math.random()*this.doesWhat.length);
+        let rand3 = Math.floor(Math.random()*this.where.length);
+        let msg = this.who[rand1] + ' ' + this.doesWhat[rand2] + ' ' + this.where[rand3] + '...';
+        console.log('\n' + msg);
     }
 };
-obj.message();
